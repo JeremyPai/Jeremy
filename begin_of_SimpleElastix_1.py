561150e8e9ec3043c33394ceaa0e8fb8b7d5be06
Jeremy Pai <jeremy.eo03@g2.nctu.edu.tw>
2021-02-21 22:06:59 +0800

    First modification

--- begin_of_SimpleElastix_1.py
+++ begin_of_SimpleElastix_1.py
@@ -1,3 +1,5 @@
 import SimpleITK as sitk
 
 resultImage = sitk.Elastix(sitk.ReadImage("fixedImage.nii"), sitk.ReadImage("movingImage.nii"), "translation")
+
+print("First time uploading the code to Github!")
